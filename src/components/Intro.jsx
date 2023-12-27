12e99b67f0561419263d3bf529fc932aa4f1e852
Yann <yannssouza@outlook.com>
2023-12-27 16:46:10 -0300

    Projeto Finalizado 5

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,32 +9,23 @@ function Intro() {
       <p className="text-base md:text-xl mb-3 font-medium">
         Engenheiro de Mixagem & Masterização.
       </p>
-      <p className="text-sm max-w-xl mb-6 font-bold">
+      <p className="text-sm max-w-2xl mb-6 font-bold">
         Olá! Sou o Yann, um engenheiro de mixagem e masterização com uma paixão
         dedicada a esculpir experiências sonoras memoráveis. Com um histórico
         sólido de 5 anos na indústria, estou comprometido em levar a sua música
-        a novas alturas, proporcionando um som envolvente e impactante.
-        Transformo suas faixas em uma mistura coesa, destacando cada elemento
-        sonoro de maneira única. Cada instrumento é habilmente equilibrado para
-        criar uma paisagem sonora rica e cativante. Elevo a sua música para um
-        nível profissional, refinando a qualidade tonal, a dinâmica e a
-        presença. O objetivo é não apenas destacar, mas aprimorar a essência
-        única de cada faixa. Trabalhei em uma variedade de gêneros musicais,
-        desde pop e rock até eletrônica e hip-hop. Minha versatilidade garante
-        que sua visão artística seja realizada, independentemente do estilo.
-        Utilizo tecnologia de última geração e ferramentas de alta qualidade
-        para garantir resultados sonoros de excelência. Cada projeto recebe
-        atenção meticulosa, usando o que há de melhor em termos de hardware e
-        software. Cada projeto é tratado com dedicação total, concentrando-me
-        nos detalhes para entregar resultados que superam expectativas. Valorizo
-        a colaboração aberta e constante com meus clientes. Seu feedback é
-        crucial, e cada ajuste é feito para garantir que a visão da sua música
-        seja realizada. Ao longo dos anos, acumulei um portfólio diversificado
-        de projetos bem-sucedidos. Muitos artistas confiaram em mim para dar
-        vida às suas criações. Se você está procurando elevar o nível sonoro da
-        sua música e criar uma experiência auditiva inesquecível, estou aqui
-        para transformar sua visão em realidade. Vamos juntos criar algo
-        extraordinário! Meu SoundCloud é esse{" "}
+        a novas alturas, proporcionando um som envolvente e impactante. Cada
+        instrumento é habilmente equilibrado para criar uma paisagem sonora rica
+        e cativante. Elevo sua música a um nível profissional, refinando
+        tonalidade, dinâmica e presença. Desde pop e rock até eletrônica e
+        hip-hop, trago minha versatilidade para cada projeto. Cada projeto
+        recebe atenção meticulosa, concentrando-me nos detalhes para entregar
+        resultados que superam expectativas. Valorizo a colaboração aberta e
+        constante, seu feedback é crucial para realizar sua visão. Ao longo dos
+        anos, acumulei um portfólio diversificado de projetos bem-sucedidos. Se
+        você está procurando elevar o nível sonoro da sua música e criar uma
+        experiência auditiva inesquecível, estou aqui para transformar sua visão
+        em realidade. Vamos juntos criar algo extraordinário! Meu SoundCloud é
+        esse{" "}
         <a
           href="https://soundcloud.com/"
           target="_blank"
